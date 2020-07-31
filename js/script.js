769f5a796e1611f1dd43533f1f0c7790d4c8ca38
ETFreeman <kfreemaneternal@gmail.com>
2020-07-31 13:57:29 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//IE HTMLcollection foreach polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+;
+{
+  var button = document.getElementById('up');
+  var prevScroll,
+      visible = false;
+  var topElem = document.querySelector('main section');
+  button.addEventListener('click', function () {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+  window.addEventListener('load', function () {
+    prevScroll = pageYOffset;
+  });
+  window.addEventListener('scroll', function () {
+    if (!visible && prevScroll) {
+      if (pageYOffset < prevScroll && topElem.getBoundingClientRect().bottom < 0) {
+        visible = true;
+        button.classList.add('visible');
+      } else {
+        visible = false;
+        button.classList.remove('visible');
+      }
+    } else visible = false;
+
+    prevScroll = pageYOffset;
+  });
+}
+;
+document.querySelectorAll('.interactive-trigger').forEach(function (item) {
+  return item.addEventListener('click', function () {
+    var target = item.getAttribute('data-target'),
+        selector = item.getAttribute('data-sel'),
+        toggleClass = item.getAttribute('data-toggleclass'),
+        detailsMode = item.getAttribute('data-detailsMode');
+    if (target == "this") target = item;
+    if (target == "parent") target = item.parentNode;
+    if (target == "grandparent") target = item.parentNode.parentNode;
+    if (!target) target = document;
+    if (selector) return target.querySelectorAll(selector).forEach(function (item) {
+      if (detailsMode && item.classList.contains('details')) {
+        if (item.style.maxHeight) item.style.removeProperty('max-height');else item.style.maxHeight = item.scrollHeight + 'px';
+      }
+
+      item.classList.toggle(toggleClass);
+    });
+    return target.classList.toggle(toggleClass);
+  });
+});
+;
+objectFitImages();
